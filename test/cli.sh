7fcb9956a42121670780f75795fe81ebf9f9af97
Joseph Werle <joseph.werle@gmail.com>
2013-04-25 08:47:49 -0400

    style changes and removed unused code

--- test/cli.sh
+++ test/cli.sh
@@ -2,27 +2,38 @@ comma=../bin/comma
 csvFile=`pwd`/example/some.csv
 outFile=`pwd`/out
 
-echo
 echo "Reading CSV file '${csvFile}' from stdin"
 #echo "cat ${csvFile} | comma"
 cat ${csvFile} | comma
 
-echo
+echo "--"
+echo "Reading CSV file '${csvFile}' from stdin and redirecting to ${outFile}"
+#echo "cat ${csvFile} | comma"
+cat ${csvFile} | comma > ${outFile}
+
+echo "Removing file ${outFile}"
+#echo "rm ${outFile}"
+rm $outFile
+
+echo "--"
 echo "Reading CSV from file '${csvFile}'"
 #echo "comma -f ${csvFile}"
 comma -f ${csvFile}
 
-echo
+echo "--"
 echo "Reading CSV from file '${csvFile}' and outputing to file ${outFile}"
 #echo "comma -f ${csvFile} -o ${outFile}"
 comma -f ${csvFile} -o $outFile
 
-echo
+echo "--"
 echo "Reading contents of file ${outFile}"
 #echo "cat ${outFile}"
 cat $outFile
-
 echo
-echo "\nRemoving file ${outFile}"
+echo "--"
+echo "Removing file ${outFile}"
 #echo "rm ${outFile}"
 rm $outFile
+
+echo "--"
+echo "finished"
